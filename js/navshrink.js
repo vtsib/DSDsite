d8214e1140858f3f625a8ee0981e17a25d017b34
vtsib <vtsibinos@gmail.com>
2017-12-10 19:38:50 +0100

    header updated and working with bootstrap 4

--- js/navshrink.js
+++ js/navshrink.js
@@ -1,8 +1,18 @@
-$(window).scroll(function() {
-	if($(document).scrollTop() > 50 ) {
+
+$(window).scroll(function () {
+    if ($(document).scrollTop() < 100) {
+ 		$('nav').removeClass('shrink');
+ 		$('.navbar .navbar-nav').removeClass('shrink');
+ 		$('.navbar-brand img').removeClass('tiny');
+ 		$('.navbar-brand img').attr('src', './images/logo_big_03.png');
+ 		//$('.navbar-brand img').attr('width', '300');
+    } else {
+	    
 		$('nav').addClass('shrink');
-	}
-	else {
-		$('nav').removeClass('shrink');
-	}
-});
+		$('.navbar .navbar-nav').addClass('shrink');
+		$('.navbar-brand img').attr('src', './images/logo_tiny_03.png');
+		$('.navbar-brand img').addClass('tiny');
+		//$('.navbar-brand img').attr('width', '60');
+
+    }
+}); 
