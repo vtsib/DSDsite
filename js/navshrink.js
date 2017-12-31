f8286b339167450deae4b678038071ffea31fcc9
vtsib <vtsibinos@gmail.com>
2017-12-31 18:29:25 +0200

    den thimamai ti exw kanei

--- js/navshrink.js
+++ js/navshrink.js
@@ -15,4 +15,47 @@ $(window).scroll(function () {
 		//$('.navbar-brand img').attr('width', '60');
 
     }
+    
+    
 }); 
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
+
+
+jQuery(document).ready(function() {
+ 
+	var offset = 250;
+ 
+	var duration = 300;
+ 
+/*	jQuery(window).scroll(function() {
+	 
+		if (jQuery(this).scrollTop() > offset) {
+	 
+			jQuery('.back-to-top').fadeIn(duration);
+	 
+		} else {
+	 
+			jQuery('.back-to-top').fadeOut(duration);
+	 
+		}
+ 
+	});
+ 
+ */
+ 
+	jQuery('.back-to-top').click(function(event) {
+	 
+		event.preventDefault();
+	 
+		jQuery('html, body').animate({scrollTop: 0}, duration);
+	 
+		return false;
+	 
+	})
+ 
+});
